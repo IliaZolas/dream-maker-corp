6fabe7d2ae6a95d717dfce88fd3176b6e68bc5a0
Ilia Zolas <ilia@thedmcsa.co.za>
2023-05-20 17:37:59 +0200

    start

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dream Maker Corp coming soon
+          Dream Maker Corp coming soon!
         </p>
         <a
           className="App-link"
